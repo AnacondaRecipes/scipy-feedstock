69c124a493b012816c427cfe8f5c0ff0c71a6488
Michael Sarahan <msarahan@gmail.com>
2017-08-31 16:50:03 -0500

    cb3 and mkl additions

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,20 +6,44 @@
 export OPENBLAS_NUM_THREADS=1
 
 
-export LIBRARY_PATH="${PREFIX}/lib"
-export C_INCLUDE_PATH="${PREFIX}/include"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+# export LIBRARY_PATH="${PREFIX}/lib"
+# export C_INCLUDE_PATH="${PREFIX}/include"
+# export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 
-# Depending on our platform, shared libraries end with either .so or .dylib
+# # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ $(uname) == 'Darwin' ]]; then
-    # Also, included a workaround so that `-stdlib=c++` doesn't go to
-    # `gfortran` and cause problems.
-    #
-    # https://github.com/conda-forge/toolchain-feedstock/pull/8
-    export CFLAGS="${CFLAGS} -stdlib=libc++ -lc++"
-    export LDFLAGS="-headerpad_max_install_names -undefined dynamic_lookup -bundle -Wl,-search_paths_first -lc++"
-else
-    unset LDFLAGS
+    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
+fi
+
+if [ "$blas_impl" = "mkl" ]; then
+
+cat > site.cfg <<EOF
+[mkl]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+mkl_libs = mkl_rt
+lapack_libs =
+
+EOF
+
+else # Non-MKL
+
+cat > site.cfg <<EOF
+[DEFAULT]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+
+[atlas]
+atlas_libs = openblas
+libraries = openblas
+
+[openblas]
+libraries = openblas
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+
+EOF
+
 fi
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
