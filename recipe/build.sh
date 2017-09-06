33130e663db24121aad367ab4bf7c5263201f641
Michael Sarahan <msarahan@gmail.com>
2017-09-06 10:34:41 -0500

    add -shared to linux; fix %fcompiler% var use in win

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,11 @@
 # on the CIs apparently.
 export OPENBLAS_NUM_THREADS=1
 
-
-# export LIBRARY_PATH="${PREFIX}/lib"
-# export C_INCLUDE_PATH="${PREFIX}/include"
-# export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-
 # # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ $(uname) == 'Darwin' ]]; then
     export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
+else
+    export LDFLAGS="$LDFLAGS -shared"
 fi
 
 if [ "$blas_impl" = "mkl" ]; then
