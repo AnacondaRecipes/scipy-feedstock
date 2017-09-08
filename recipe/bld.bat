fabb454bd594b1e8e0d2688ebf4cfdd10b08786f
Michael Sarahan <msarahan@gmail.com>
2017-09-08 14:54:13 -0500

    windows fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,28 +1,34 @@
+if errorlevel 1 exit 1
+
+:: bug in VS215 update 3 crashes with internal compiler error.  Advice from MS is to disable LTCG.
+(
+echo [ALL]
+echo library_dirs = %LIBRARY_LIB%
+echo include_dirs = %LIBRARY_INC%
+) >> site.cfg
+
+
+if "%c_compiler%" == "vs2015" (
+echo extra_link_args = /LTCG:OFF >> site.cfg
+)
+
 if "%blas_impl%" == "mkl" (
 
 (
 echo [mkl]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
 echo mkl_libs = mkl_core_dll, mkl_intel_lp64_dll, mkl_intel_thread_dll
-echo lapack_libs = mkl_lapack95_lp64
+echo lapack_libs = mkl_scalapack_lp64_dll
 ) > site.cfg
 
 ) else (
 
 (
-echo [DEFAULT]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo.
 echo [atlas]
 echo atlas_libs = openblas
 echo libraries = openblas
 echo.
 echo [openblas]
 echo libraries = openblas
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
 ) > site.cfg
 
 )
@@ -33,5 +39,5 @@ IF "%target_platform%" == "win-64" (
   set "fcompiler=intel"
 )
 
-python setup.py config --fcompiler=%fcompiler% build_clib --fcompiler=%fcompiler% build_ext --fcompiler=%fcompiler% install
+python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
