9702fbac7487a2b1f23450240095c8fd1f9ec4c4
Michael Sarahan <msarahan@gmail.com>
2017-09-11 22:39:36 -0500

    remove site.cfg stuff; baked into numpy. comment test patch for now.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,43 +1,6 @@
 if errorlevel 1 exit 1
 
-:: bug in VS215 update 3 crashes with internal compiler error.  Advice from MS is to disable LTCG.
-(
-echo [ALL]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-) >> site.cfg
-
-
-if "%c_compiler%" == "vs2015" (
-echo extra_link_args = /LTCG:OFF >> site.cfg
-)
-
-if "%blas_impl%" == "mkl" (
-
-(
-echo [mkl]
-echo mkl_libs = mkl_core_dll, mkl_intel_lp64_dll, mkl_intel_thread_dll
-echo lapack_libs = mkl_scalapack_lp64_dll
-) > site.cfg
-
-) else (
-
-(
-echo [atlas]
-echo atlas_libs = openblas
-echo libraries = openblas
-echo.
-echo [openblas]
-echo libraries = openblas
-) > site.cfg
-
-)
-
-IF "%target_platform%" == "win-64" (
-  set "fcompiler=intelem"
-) else (
-  set "fcompiler=intel"
-)
+:: site.cfg unnecessary here; it is already baked into numpy
 
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
