5383fe36b120fbb3cb36636bdb044d9e4af4945d
Michael Sarahan <msarahan@gmail.com>
2018-12-14 09:03:07 -0600

    fix site.cfg usage; upper bound on pytest; xfail mkl mac test

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,35 +12,6 @@ else
     export LDFLAGS="$LDFLAGS -shared"
 fi
 
-if [ "$blas_impl" = "mkl" ]; then
-
-cat > site.cfg <<EOF
-[mkl]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-mkl_libs = mkl_rt
-lapack_libs =
-
-EOF
-
-else # Non-MKL
-
-cat > site.cfg <<EOF
-[DEFAULT]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-[atlas]
-atlas_libs = openblas
-libraries = openblas
-
-[openblas]
-libraries = openblas
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-EOF
-
-fi
+cp $PREFIX/site.cfg site.cfg
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
