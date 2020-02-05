fd200ec4f3b53d56296cc30357763d5acadc6743
Jonathan Helmus <jjhelmus@gmail.com>
2020-02-05 16:43:19 -0600

    add -fno-lto to CFLAGS on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ export OPENBLAS_NUM_THREADS=1
 # # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ $(uname) == 'Darwin' ]]; then
     export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
+    export CFLAGS="$CFLAGS -fno-lto"
 else
     export LDFLAGS="$LDFLAGS -shared"
 fi
