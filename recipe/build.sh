c80dde976a2a3c92e5cf17a51f424f6ada10365a
Kai Tietz <ktietz@anaconda.com>
2021-08-25 17:46:23 +0200

    update to version 1.7.1 for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use the G77 ABI wrapper everywhere so that the underlying blas implementation
+# can have a G77 ABI (currently only MKL)
+export SCIPY_USE_G77_ABI_WRAPPER=1
 
 # Use OpenBLAS with 1 thread only as it seems to be using too many
 # on the CIs apparently.
