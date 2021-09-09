57ed39b60be55204192103f13d807d8e60400599
Anthony DiPietro <adipietro@anaconda.com>
2021-09-09 14:20:11 -0400

    Revert "Update environment variables for win-32."
    This reverts commit 09272671349b250005e98720000cb66debba290f.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -37,11 +37,12 @@ clang-cl.exe --version
 if %ERRORLEVEL% neq 0 exit 1
 
 REM set compilers to clang-cl
-set "CC=clang-cl.exe"
-set "CXX=clang-cl.exe"
-
-if %ARCH% == 32 (
-  set "CXXFLAGS= -m32 "
+if %ARCH% == 64 (
+  set "CC=clang-cl.exe"
+  set "CXX=clang-cl.exe"
+) else (
+  set "CC=clang-cl.exe -m32"
+  set "CXX=clang-cl.exe -m32"
 )
 
 REM clang-cl & gfortran use different LDFLAGS; unset it
