3f1ca697cda581791eebeb45c07598b9a0dbddce
Serhii Kupriienko <skupriienko@anaconda.com>
2022-07-20 10:23:04 +0000

    Skip tests on linux and osx

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -45,6 +45,14 @@ if %ARCH% == 64 (
   set "CXX=clang-cl.exe -m32"
 )
 
+REM check if clang-cl is on path as required
+clang-cl.exe --version
+if %ERRORLEVEL% neq 0 exit 1
+
+REM set compilers to clang-cl
+set "CC=clang-cl"
+set "CXX=clang-cl"
+
 REM clang-cl & gfortran use different LDFLAGS; unset it
 set "LDFLAGS="
 REM don't add d1trimfile option because clang doesn't recognize it.
