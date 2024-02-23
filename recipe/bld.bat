7b2b0e70127322509181efb53831bc1e573487ca
Charles Bousseau <cbousseau@anaconda.com>
2024-02-23 16:04:15 -0500

    update to 1.12.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,34 +19,6 @@ set "LDFLAGS="
 REM don't add d1trimfile option because clang doesn't recognize it.
 set "SRC_DIR="
 
-
-
-REM switch between numpy.distutil build and meson build
-if "%PY_VER%" == "3.12" goto :mesonbuild
-
-REM Align numpy intel fortran compiler flags with what is expected by scipy.
-REM Regarding /fp:strict and /assume:minus0 see: https://github.com/scipy/scipy/issues/17075 
-REM Regarding /fpp The pre-processor flag is not enabled on older numpy.
-powershell -Command "(gc %SP_DIR%\numpy\distutils\fcompiler\intel.py) -replace '''/nologo'', ', '''/nologo'', ''/fpp'', ''/fp:strict'', ''/assume:minus0'', ' | Out-File -encoding ASCII %SP_DIR%\numpy\distutils\fcompiler\intel.py"
-if errorlevel 1 exit 1
-
-COPY %PREFIX%\site.cfg site.cfg
-
-REM Use the G77 ABI wrapper everywhere so that the underlying blas implementation
-REM can have a G77 ABI (currently only MKL)
-set SCIPY_USE_G77_ABI_WRAPPER=1
-
-REM add %PREFIX%\include for inclusion of Python.h in linalg
-set "INCLUDE=%INCLUDE%;%PREFIX%\include"
-
-%PYTHON% _setup.py install --single-version-externally-managed --record=record.txt
-if %ERRORLEVEL% neq 0 exit 1
-exit 0
-
-
-
-:mesonbuild
-
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 if "%blas_impl%" == "openblas" (
     set "BLAS=openblas"
